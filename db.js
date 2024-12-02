3480d91a9601c330c18f86f334b188d8c4198c34
MrJ8585 <josea.solis@cetys.edu.mx>
2024-12-02 01:21:15 -0800

    Updated db.js

--- db.js
+++ db.js
@@ -1,23 +1,7 @@
 const sql = require("mssql");
 require("dotenv").config();
 
-const dbConfig = {
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	server: process.env.DB_SERVER,
-	database: process.env.DB_NAME,
-	options: {
-		encrypt: process.env.DB_ENCRYPT === "true",
-		trustServerCertificate:
-			process.env.DB_TRUST_SERVER_CERTIFICATE === "true",
-		loginTimeout: parseInt(process.env.DB_LOGIN_TIMEOUT, 10) || 30000,
-	},
-	pool: {
-		max: 10,
-		min: 0,
-		idleTimeoutMillis: 30000,
-	},
-};
+const connectionString = process.env.SQL_CONNECTION_STRING;
 
 // Exportar la conexión
 let pool;
@@ -25,7 +9,7 @@ let pool;
 const getDbConnection = async () => {
 	if (!pool) {
 		try {
-			pool = await sql.connect(dbConfig);
+			pool = await sql.connect(connectionString);
 			console.log("Conexión a SQL Server establecida");
 		} catch (err) {
 			console.error("Error al conectar a SQL Server", err);
