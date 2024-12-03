81f089858dacfb292b2e4f3757534e679c91fa0a
Caleb Payan <calebpayan75@gmail.com>
2024-12-02 17:36:57 -0800

    Farm by user Id controller updated

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const router = require("./controller"); // Importa el enrutador exportado en controller.js
+const cors = require("cors");
+
+const morgan = require("morgan");
 
 dotenv.config();
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(cors());
 
 app.use(express.json()); // Middleware para manejar JSON
 
@@ -13,14 +20,14 @@ app.use("/api", router); // Prefijo /api para las rutas (puedes cambiarlo si lo 
 
 // Manejo de errores genérico
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({
-		error: "Algo salió mal, por favor intente más tarde.",
-	});
+  console.error(err.stack);
+  res.status(500).json({
+    error: "Algo salió mal, por favor intente más tarde.",
+  });
 });
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-	console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
